ea1aedbc95764745fa5e13e8d1eb3ba17f82f90c
Francisco <37129497+Rayo-Fco@users.noreply.github.com>
2021-04-29 20:13:15 -0400

    Create index.ts

---
+++ src/Config/index.ts
@@ -0,0 +1,22 @@
+export default 
+{
+    port: process.env.PORT || 3000,
+    database: {
+        URI: process.env.MONGODB_URI || 'mongodb://localhost/api-ts',
+        USER: process.env.MONGODB_USER,
+        PASSWORD: process.env.MONGODB_PASSWORD
+      },
+    SECRET_TOKEN: "KeyApiTypeScript",
+    Password_Salt: 10,
+    mail:{
+      host: '',
+      port: 587,
+      user: '',
+      pass: 'A193756842'
+    },
+    tienda:{
+      nombre:' Lenceria pruebaa',
+      dominio: 'http://localhost:3000'
+    }
+
+}
